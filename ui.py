1dbc28aa80ab894563b6d835158776f64ea97c8a
PawelSyktus <pawel.syktus@gmail.com>
2017-06-06 21:28:52 +0200

    Add function make_table in ui.py module

--- ui.py
+++ ui.py
@@ -1,6 +1,69 @@
+def make_table(table):
+
+    columns = len(table[0])
+    rows = len(table)
+    columns_rows = []
+    longests = []
+    columns_rows_with_whitespace = []
+
+    for i in range(0, columns):
+        columns_rows.append([])
+        columns_rows_with_whitespace.append([])
+
+    for i in range(columns):
+        for j in range(rows):
+            columns_rows[i].append(table[j][i])
+
+    for i in range(len(columns_rows)):
+
+        highest = len(str(columns_rows[i][0]))
+
+        for element in columns_rows[i]:
+            if highest < len(str(element)):
+                highest = len(str(element))
+        longests.append(highest)
+
+    for i in range(len(columns_rows)):
+        for element in columns_rows[i]:
+            # len_difference = len(' '*((longests[i]-len(str(element)))))
+            len_difference = longests[i] - len(str(element))
+            rounded_up = ' '*int(len_difference/2)
+
+            if len(str(element)) == longests[i]:
+                element = '|  ' + str(element) + '  |'
+            else:
+                if (longests[i] - len(str(element))) % 2 == 0:
+                    element = '|  ' + rounded_up + str(element) + rounded_up + '  |'
+                else:
+                    element = '|  ' + rounded_up + str(element) + rounded_up + ' ' + '  |'
+            columns_rows_with_whitespace[i].append(element)
+
+    back_to_rows = []
+
+    for i in range(0, rows):
+        back_to_rows.append([])
+
+    for i in range(len(back_to_rows)):
+        for j in range(len(columns_rows_with_whitespace)):
+            back_to_rows[i].append(columns_rows_with_whitespace[j][i])
+
+    board_lines = []
+
+    for element in longests:
+        line = '|' + (element * '-') + '|'
+        board_lines.append(line)
+
+    for element in back_to_rows:
+        for element1 in element:
+            print(element1, end='')
+        print()
+
+    table = back_to_rows
+    return table
 
 
 def print_table(table, title_list):
+
     """
     Prints table with data. Sample output:
         /-----------------------------------\
@@ -18,10 +81,12 @@ def print_table(table, title_list):
     Returns:
         This function doesn't return anything it only prints to console.
     """
+    # data = [titles] + list(zip(key, values))
 
-    # your goes code
-
-    pass
+    for row in table:
+        for element in row:
+            print(element, end='')
+        print()
 
 
 def print_result(result, label):
@@ -88,8 +153,6 @@ def get_inputs(list_labels, title):
     """
     inputs = []
 
-    # your code
-
     return inputs
 
 
@@ -110,3 +173,22 @@ def print_error_message(message):
     # your code
 
     pass
+
+####################################################
+# TEGO MAINA TUTAJ NIE MA! TYLKO DO CELOW TESTOWYCH!
+
+
+def main():
+    table = [['1dsa', 'Janusz Kozlowski', 1922234], ['dakdjhkajhdw', 'Barbra Saasdatreid', 1991122],
+             ['dkllk7', 'AlbadasSadasd', 1929929921223], ['dkllk798820', 'AlbadasSasdasadasd', 192992992122322],
+             ['dakdjhkajhdw', 'Barbra Saasdatreid', 1991122], ['dakdjhkajhdw', 'Barbra Saasdatreid', 1991122]]
+
+    title_list = ['ID', 'NAME', 'YEAR']
+
+    make_table(table)
+
+main()
+
+
+# TEGO MAINA TUTAJ NIE MA! TYLKO DO CELOW TESTOWYCH!
+####################################################
