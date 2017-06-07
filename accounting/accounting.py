59cf63eb0ec785a536aad7ea113fd765dcbdcde0
Grzegorz Winiarek <g.d.winiarek@gmail.com>
2017-06-07 14:56:59 +0200

    Added update and which year max functions

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -99,23 +99,25 @@ def update(table, id_):
         table with updated record
     """
 
-    inputs = ['Enter month: ', 'Enter day: ', 'Enter year:', 'Enter type: ', 'Enter amount: ']
-    inputs_entered = ui.get_inputs(inputs,'Update your record')
-
-
-    for element in table:
-        if element[0] == id_:
-            for j in range(0,5):
-                element[j+1] = inputs_entered[j]
-
-    data_manager.write_table_to_file("customers.csv", table)
-
-    return table
-
-
-table = data_manager.get_table_from_file("items.csv")
-us_input = ui.get_inputs(['Enter ID: '],"hello")
+#     inputs = ['Enter month: ', 'Enter day: ', 'Enter year:', 'Enter type: ', 'Enter amount: ']
+#     inputs_entered = ui.get_inputs(inputs,'Update your record')
+#
+#
+#     for element in table:
+#         if element[0] == id_:
+#             for j in range(0,5):
+#                 element[j+1] = inputs_entered[j]
+#
+#     data_manager.write_table_to_file("customers.csv", table)
+#
+#     return table
+#
+#
+# table = data_manager.get_table_from_file("items.csv")
+# us_input = ui.get_inputs(['Enter ID: '],"hello")
 #print(update(table, us_input[0]))
+
+    pass
 
 
 
@@ -126,9 +128,47 @@ us_input = ui.get_inputs(['Enter ID: '],"hello")
 # return the answer (number)
 def which_year_max(table):
 
-    # your code
+    # in_list_2016 = []
+    # out_list_2016 = []
+    # in_list_2015 = []
+    # out_list_2015 = []
 
-    pass
+    profit = {}
+
+
+    for elements in table:
+#        for year in range(1990,2018):
+            #print(year)
+            if elements[3] not in profit:
+                profit.update({elements[3] : 0})
+                if elements [4] == "in":
+                    profit[elements[3]] += (int(elements[5]))
+                elif elements[4] == "out":
+                    profit[elements[3]] += (-1 * int(elements[5]))
+            elif elements[3] in profit:
+                if elements [4] == "in":
+                    profit[elements[3]] += (int(elements[5]))
+                elif elements[4] == "out":
+                    profit[elements[3]] += (-1 * int(elements[5]))
+    #
+    #     elif elements[3] == "2015":
+    #         if elements [4] == "in":
+    #             in_list_2015.append(int(elements[5]))
+    #         elif elements[4] == "out":
+    #             out_list_2015.append(int(elements[5]))
+    #
+    # profit_2016 = sum(in_list_2016) - sum(out_list_2016)
+    # profit_2015 = sum(in_list_2015) - sum(out_list_2015)
+    #
+    # if profit_2015 > profit_2016:
+    #     answer = "2015"
+    # elif profit_2016 > profit_2015:
+    #     answer = "2016"
+
+    print(profit)
+
+table = data_manager.get_table_from_file("items.csv")
+print(which_year_max(table))
 
 
 # the question: What is the average (per item) profit in a given year? [(profit)/(items count) ]
